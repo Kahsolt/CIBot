7bd09af61f913a318476ddcca7d66334d65a2e2f
visual233 <264050673@qq.com>
2018-01-21 19:33:14 +0800

    add doctor account

--- lib/wxBot/wxbot_adapter.py
+++ lib/wxBot/wxbot_adapter.py
@@ -105,6 +105,12 @@ class QaBot(WXBot):
             u'gender': 0,
         }
         data['users'].append(u)
+        u = {
+            u'id': 'doctor0',
+            u'nickname': 'doctor0',
+            u'gender': 0,
+        }
+        data['users'].append(u)
         for u in self.contact_list:
             u = {
                 u'id': u.get('UserName'),
@@ -123,7 +129,8 @@ class QaBot(WXBot):
         try:
             requests.post(url=URL_USER, json=data)
             print('[Contact] Sent %d contacts info :)' % len(data['users']))
-        except:
+        except Exception as e:
+            print(e)
             print('[Contact] You live alone on the island, uha? :(')
 
     def send_question(self, uid, quest):
